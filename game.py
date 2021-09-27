defdb45dc640e8fc848fddee34908f653fb180f7
John Bradford <jwgbradford@gmail.com>
2021-09-27 18:07:19 +0100

    added simple colour draw to follow base data

--- game.py
+++ game.py
@@ -1,18 +1,42 @@
 from random import randint
+import pygame
+
+COLOURS = [ (0, 0, 0),
+    (255, 0, 0), (255, 64, 0), (255, 128, 0), (255, 191, 0), 
+    (255, 255, 0), (191, 255, 0), (128, 255, 0), (64, 255, 0), 
+    (0, 255, 0), (0, 255, 64), (0, 255, 128), (0, 255, 191),
+    (0, 255, 255), (0, 191, 255), (0, 128, 255), (0, 64, 255), (0, 0, 255)
+    ]
 
 class Jewel():
     def __init__(self, pos) -> None:
         self.x, self.y = pos
-        self.colour = randint(1, 6)
+        self.colour = randint(1, len(COLOURS) - 1)
+        self.make_image()
+        self.get_rect()
         self.reset_triples()
 
     def reset_triples(self):
         self.vertical_triple, self.horizontal_triple = [], []
         
+    def make_image(self):
+        image = pygame.Surface((40, 40))
+        image.set_colorkey(COLOURS[0])  # Black colors will not be blit.
+        pygame.draw.circle(image, COLOURS[self.colour], (20, 20), 20)
+        self.my_image =  image
+    
+    def get_rect(self):
+        jewel_center = (20 + (self.x * 40), 20 + (self.y * 40))
+        jewel_rect = self.my_image.get_rect(center = jewel_center)
+        self.my_rect =  jewel_rect
+
 class Engine():
     def __init__(self) -> None:
         self.board_width = 10
         self.board_height = 10
+        pygame.init()
+        self.game_window = pygame.display.set_mode((self.board_width * 40, self.board_height * 40))
+        pygame.display.set_caption("DigiJewels")
         self.game_board = [[Jewel((i, j)) for i in range(self.board_width)] for j in range(self.board_height)]
 
     def run(self):
@@ -21,6 +45,7 @@ class Engine():
             print('')
             self.find_triples()
             self.print_board()
+            self.draw_board()
             self.board_drop()
             pause = input()
 
@@ -31,7 +56,9 @@ class Engine():
                     if i > 0:
                         for x in range(i):
                             self.game_board[i - x][j].colour = self.game_board[i - x - 1][j].colour
-                    self.game_board[0][j] = Jewel((0, j))
+                            self.game_board[i - x][j].make_image()
+                            self.game_board[i - x][j].get_rect()
+                    self.game_board[0][j].colour = randint(1, len(COLOURS) - 1)
 
     def find_triples(self):
         for i, row in enumerate(self.game_board):
@@ -66,6 +93,13 @@ class Engine():
                     for cell in jewel.vertical_triple:
                         x, y = cell
                         self.game_board[x][y].colour = 0
+
+    def draw_board(self):
+        self.game_window.fill(COLOURS[0])
+        for row in self.game_board:
+            for jewel in row:
+                self.game_window.blit(jewel.my_image, jewel.my_rect)
+        pygame.display.update()
 
     def print_board(self):
         for row in self.game_board:
